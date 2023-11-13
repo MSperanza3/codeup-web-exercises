9659d045e0fd66dbb9c89a2bd440c23beba5181d
Matteo Speranza <matteo.u.speranza@gmail.com>
2023-11-13 11:59:48 -0600

    added first part of Excersie 3 into external_js

--- js/external.js
+++ js/external.js
@@ -10,4 +10,10 @@ console.log(`User Input was ${userInput}`
 )
 let messageColor =`Great, ${userInput} is my favorite color too!`;
 alert(messageColor)
-let
+let littleMermaidRentTime = 3;
+let brotherBearRentTime = 5;
+let herculesRentTime = 1;
+let pricePerMovie = 3;
+let totalAmountForMovies = (littleMermaidRentTime + brotherBearRentTime + herculesRentTime) * pricePerMovie
+console.log(`Total amount spent for movies is $${totalAmountForMovies}!`)
+
