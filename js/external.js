c50cf4c2aa902ee562790405c28bcb1440a7737c
Matteo Speranza <matteo.u.speranza@gmail.com>
2023-11-13 11:50:46 -0600

    created and added text to external_js

---
+++ js/external.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let messageExternal ="Hello from External JS";
+console.log(messageExternal
+);
+let userChoice =confirm('Welcome to my Website!');
+console.log(`The user's choice was: ${userChoice}`)
+let userInput =prompt("What is you're favorite color?");
+console.log(`User Input was ${userInput}`
+)
+let messageColor =`Great, ${userInput} is my favorite color too!`;
+alert(messageColor)
