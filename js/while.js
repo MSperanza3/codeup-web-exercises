c570e2559064d0eedb67a02dd6e407df8f763e8c
Matteo Speranza <matteo.u.speranza@gmail.com>
2023-11-16 16:38:13 -0600

    finished all problems

---
+++ js/while.js
@@ -0,0 +1,23 @@
+"use strict"
+// let number = 2
+// let exponent = 1
+// while (exponent <= 16) {
+//     console.log(number);
+//     number *= 2;
+//     exponent++
+// }
+// let allCones = Math.floor(Math.random() * 50) + 50
+//
+// console.log(`Cones to sell today is ${allCones}`);
+// let conesSold = 0
+// do {
+//     let conesBought =Math.floor(Math.random() * 5) + 1;
+//     if (conesBought <= allCones) {
+//     allCones -= conesBought
+//         conesSold += conesBought
+//         console.log(`Cones purchaced was ${conesBought} and we have ${allCones} left! `)
+//     } else {
+//         console.log(`Sorry we dont have enough cones! Remaining Cones: ${allCones}`)
+//     }
+// } while(allCones > 0);
+// console.log(`All cones are Sold!`)
