ed09d69f6f4e40825664bebef35a9fbe27dd8ebf
Matteo Speranza <matteo.u.speranza@gmail.com>
2023-11-21 10:59:25 -0600

    finsihed circles and objects excersies

---
+++ js/circle.js
@@ -0,0 +1,29 @@
+
+(function() {
+    "use strict";
+    let circle = {
+        radius: 5,
+
+        getArea: function () {
+            let area = Math.PI * Math.pow(this.radius, 2)
+            return area;
+        },
+
+        logInfo: function (doRounding) {
+            let area = this.getArea();
+            if (doRounding) {
+                area = Math.round(area)
+            }
+
+            console.log(`Area of a circle with radius: ${this.radius}, is: ${area}`);
+        }
+    };
+
+    // log info about the circle
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
+
+    console.log("=======================================================");
+})();
